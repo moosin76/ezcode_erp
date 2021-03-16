110d9302783a450dd16895e2e64fcd6aa9fa3ac4
Kiyong Nam <moosin76@gmail.com>
2021-03-16 18:06:52 +0900

    firebase 초기화

---
+++ src/plugins/firebase.js
@@ -0,0 +1,10 @@
+
+
+const firebaseConfig = {
+	apiKey: "AIzaSyBmt7enJ1lmqfEXUI8Lswu0we0It-wHu2w",
+	authDomain: "ezcode-erp.firebaseapp.com",
+	projectId: "ezcode-erp",
+	storageBucket: "ezcode-erp.appspot.com",
+	messagingSenderId: "283037624378",
+	appId: "1:283037624378:web:63f690fec8504f3d1d2d03"
+};
