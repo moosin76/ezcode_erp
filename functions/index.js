9aa3520a1215daf28fb7c531a4d06780f4f90350
Kiyong Nam <moosin76@gmail.com>
2021-03-17 20:57:14 +0900

    펑션스 write권한 세팅

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,7 @@ var serviceAccount = require("./ezcode-erp-admin-key.json");
 
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
-	databaseURL: "https://ezcode-erp-default-rtdb.firebaseio.com"
+	databaseURL: functions.config().admin.db_url // "https://ezcode-erp-default-rtdb.firebaseio.com"
 });
 
 // exports.helloWorld = functions.https.onRequest((request, response) => {
@@ -19,11 +19,16 @@ const db = admin.database();
 // 계정이 생성될 때 
 exports.createUser = functions.auth.user().onCreate(async (user) => {
 	const { uid, email, displayName, photoURL } = user;
+
+	const now = new Date();
+	now.setHours(now.getHours()+9); // 서울 지역으로
+	const date = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
 	const mb = {
 		email: email,
 		displayName: displayName,
 		photoURL: photoURL,
-		createAt: new Date()
+		createAt: date,
+		level: email == functions.config().admin.email ? 10 : 0
 	};
 	db.ref('users').child(uid).set(mb);
 });
